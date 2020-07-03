c1435e477ea83f98a6d0c1d7b355fb7dd0aa62a9
kirill kirill <grosukirill009@gmail.com>
2020-07-03 15:37:05 +0300

    f

---
+++ build/precache-manifest.db8bf6015125ea9ee1e497677ea45e26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3172514ed49f2f814c7d5aac6bfa226",
+    "url": "/vk-app/index.html"
+  },
+  {
+    "revision": "bde6ac612eadd5ef1af9",
+    "url": "/vk-app/static/css/2.a28d9ceb.chunk.css"
+  },
+  {
+    "revision": "bde6ac612eadd5ef1af9",
+    "url": "/vk-app/static/js/2.a952d8cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk-app/static/js/2.a952d8cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6d26b1d7f8cf6492a6",
+    "url": "/vk-app/static/js/main.dc750bf5.chunk.js"
+  },
+  {
+    "revision": "41cdca29cf33598da147",
+    "url": "/vk-app/static/js/runtime-main.5d036902.js"
+  },
+  {
+    "revision": "92efc121d71105df65cd4400e8f82a3b",
+    "url": "/vk-app/static/media/UJAnRhJ.92efc121.gif"
+  },
+  {
+    "revision": "2909855557c212147a027fa0b45e324b",
+    "url": "/vk-app/static/media/push_ups_new.29098555.png"
+  }
+]);
